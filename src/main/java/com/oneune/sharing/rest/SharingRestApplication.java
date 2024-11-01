82684fbfdcd5436dfb984d9533b7c1df7eb4c086
1unee <babnicshev.back@mail.ru>
2024-11-01 16:14:55 +0300

    init commit

---
+++ src/main/java/com/oneune/sharing/rest/SharingRestApplication.java
@@ -0,0 +1,13 @@
+package com.oneune.sharing.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oneune/sharing/rest/SharingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneune.sharing.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
